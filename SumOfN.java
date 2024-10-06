76d9e3009590ffc4736bdd0bf7909dd31134ac26
Anisha Patil <163636492+anishapatil05@users.noreply.github.com>
2024-10-06 07:28:37 +0530

    Add files via upload

---
+++ SumOfN.java
@@ -0,0 +1,9 @@
+// Sum of N natural numbers
+import java.util.*;
+public class SumOfN{
+    public static void main(String[] args){
+        int n=5;
+         int result= n*(n+1)/2;
+         System.out.println(result);
+    }
+}
